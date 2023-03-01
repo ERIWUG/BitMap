e0b9bdf965c4009241fef3b4dcc2d024a1995f93
ERIWUG <51087583+ERIWUG@users.noreply.github.com>
2023-03-01 14:40:37 +0300

    Added base program
    Create program which convert bitmap image to massive of PictureBox and dynamically represents any changes.

---
+++ Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(356, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 209);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(468, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(793, 675);
+            this.panel1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Bitmap files (*.bmp)|*.bmp|Image files(*.jpg)|*.jpg";
+            this.openFileDialog1.InitialDirectory = "images";
+            this.openFileDialog1.Title = "Выберете нужый файл";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(13, 354);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(379, 333);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(273, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 699);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab1/Lab1/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+
+    {
+        public PictureBox[,] pbs = new PictureBox[50, 50];
+        public byte[,] pix = new byte[50, 50];
+        public Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private PictureBox Cr_PB(int i, int j, bool fl)
+        {
+            PictureBox pictureBox2 = new PictureBox();
+            if (fl)
+            {
+                pictureBox2.BackColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                pictureBox2.BackColor = System.Drawing.Color.White;
+            }
+            pictureBox2.Location = new System.Drawing.Point(10*i, 10*j);
+            pictureBox2.Name = "PB_"+Convert.ToString(i)+"_"+Convert.ToString(j);
+            pictureBox2.Size = new System.Drawing.Size(10, 10);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += new System.EventHandler(pictureBox2_Click);
+            pictureBox2.MouseHover += new System.EventHandler(pictureBox2_MouseHover);
+            return pictureBox2; 
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            } 
+            bmp = new Bitmap(pictureBox1.Image);
+            
+            
+            PictureBox pb;
+            for(int i = 0; i < 50; i++)
+            {
+                for(int j = 0; j < 50; j++)
+                {
+                    
+                    if (bmp.GetPixel(i, j).ToArgb() == -1)
+                    {
+                        pix[i, j] = 0;
+                    }
+                    else
+                    {
+                        pix[i, j] = 1;
+                    }
+
+                }
+            }
+
+
+            for(int i = 0; i < 50; i++)
+            {
+                for(int j = 0; j < 50; j++)
+                {
+                    pb = Cr_PB(i, j, pix[i, j] == 1);
+                    pbs[i, j] = pb;
+                    panel1.Controls.Add(pbs[i,j]);
+                }
+            }
+
+            pictureBox2.Image = bmp;
+
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            var pb = sender as PictureBox;
+            if(pb.BackColor == Color.White)
+            {
+                pb.BackColor = Color.Black;
+            }
+            else
+            {
+                pb.BackColor = Color.White;
+            }
+            int x; int y;
+
+            String[] l = pb.Name.Remove(0, 3).Split('_');
+            x = Convert.ToInt16(l[0]);
+            y = Convert.ToInt16(l[1]);
+
+            if (pix[x, y]==0)
+            {
+                pix[x, y] = 1;
+                bmp.SetPixel(x, y, Color.Black);
+            }
+            else
+            {
+                pix[x, y] = 0;
+                bmp.SetPixel(x, y, Color.White);
+            }
+            pictureBox2.Image = bmp;
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) //если в диалоговом окне нажата кнопка "ОК"
+            {
+                try
+                {
+                    bmp.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            int x;int y;
+            var pb = sender as PictureBox;
+            String[] l = pb.Name.Remove(0, 3).Split('_');
+            x = Convert.ToInt16(l[0]);
+            y = Convert.ToInt16(l[1]);
+
+            int k4 = 0;
+            int k8 = 0;
+            try
+            {
+                if (pbs[x - 1, y - 1].BackColor == Color.Black) { k8++; }
+                
+            }
+            catch (IndexOutOfRangeException){ }
+            try
+            {
+                if (pbs[x - 1, y].BackColor == Color.Black) {k4++; k8++;}
+            
+                
+            }
+            catch (IndexOutOfRangeException) { }
+            try
+            {
+                if (pbs[x - 1, y + 1].BackColor == Color.Black) k8++;
+            }
+            catch (IndexOutOfRangeException) { }
+            try
+            {
+                if (pbs[x, y - 1].BackColor == Color.Black) { k4++; k8++; }
+            }
+            catch (IndexOutOfRangeException) { }
+            try
+            {
+                if (pbs[x , y + 1].BackColor == Color.Black){ k4++; k8++;}
+            }
+            catch (IndexOutOfRangeException) { }
+            try
+            {
+                if (pbs[x + 1, y - 1].BackColor == Color.Black) k8++;
+            }
+            catch (IndexOutOfRangeException) { }
+            try
+            {
+                if (pbs[x + 1, y].BackColor == Color.Black) {k4++; k8++;
+            }
+            }
+            catch (IndexOutOfRangeException) { }
+            try
+            {
+                if (pbs[x + 1, y + 1].BackColor == Color.Black)  k8++;
+            }
+            catch (IndexOutOfRangeException) { }
+
+            textBox1.Text = "K4: " + Convert.ToString(k4) + "  k8: " + Convert.ToString(k8);
+
+
+        }
+    }
+}
